3c55a3e587aa45d6491d99b514d7e29a988163bb
SHUBHAM VAISHNAV <shubham.vaishnav@capgemini.com>
2020-06-14 15:08:14 +0530

    Initial Commit

---
+++ RealmApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmApp2
+//
+//  Created by Pranjal Singhai on 14/06/20.
+//  Copyright © 2020 Capgemini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
